90a4c5e25081766f7784586efb3df6e61b631dde
marco <marco.bahn@gmail.com>
2017-07-24 20:02:51 +0200

    add components/header

---
+++ src/components/organisms/Header/index.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+import {
+  Navigation,
+} from 'components'
+
+const Header = () => {
+  return (
+    <div className="header">
+      <Navigation />
+    </div>
+  )
+}
+
+export default Header
